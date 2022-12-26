cb0df55063bd6c972c558f492796e7c7221bab17
gunjan-sachdeva <58691180+gunjan-sachdeva@users.noreply.github.com>
2022-12-26 16:52:35 +0530

    Revert "contact"
    This reverts commit f18973ba7166e4ef0e26d6e518b38aee8a0c8a4f.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,7 @@ export class Contact extends Component {
   render() {
     return (
       <div>
-     <p>My contact page</p> 
-     <h1>Sign Up page</h1>  
+     <p>My contact page</p>   
       </div>
     )
   }
